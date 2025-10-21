1223e832d71451d627529c1605faef4ba1d5566a
Mookid47 <marcosdiazortiz@hotmail.com>
2025-10-21 20:52:37 -0300

    Se cargó el DOM en main.js para editar elementos, tambien puse un evento al clickear un marcador como prueba

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", mainFunction);
+
+function mainFunction(){
+    console.log("Documento cargado y listo.");
+
+    // Cargamos el mapa y le agregamos un evento de click
+    let map = document.getElementById("map");
+    map.addEventListener("click", function(){
+        console.log("Mapa clickeado.");
+    });
+}
+
+
--- js/mapa.js
+++ js/mapa.js
@@ -1,9 +1,17 @@
-var map = L.map('map').setView([-34.543032, -58.711943], 13);
+var map = crearMapa();
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
+//Se agrega un marcador y un evento al clickearlo
 var marker = L.marker([-34.543032, -58.711943]).addTo(map);
+marker.addEventListener('click', function() {
+    alert("Centro de Asistencia")
+});
 
+function crearMapa() {
+    var map = L.map('map').setView([-34.543032, -58.711943], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    return map;
+}
